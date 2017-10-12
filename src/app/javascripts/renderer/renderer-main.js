1a4217730dc2fda29ab476acf57f9d5987af3ff9
ktp <t08265hk@gmail.com>
2017-10-12 16:20:44 +0900

    [add] form for user name and use firebase npm pkg.

--- src/app/javascripts/renderer/renderer-main.js
+++ src/app/javascripts/renderer/renderer-main.js
@@ -1,18 +1,50 @@
 // Initialize Firebase
-const config = {
+var config = {
+  apiKey: "AIzaSyB_slMURwrAuf0JvCQCCwZw8SqMdVdHEfA",
+  authDomain: "sudachi-47f40.firebaseapp.com",
+  databaseURL: "https://sudachi-47f40.firebaseio.com",
+  projectId: "sudachi-47f40",
+  storageBucket: "sudachi-47f40.appspot.com",
+  messagingSenderId: "112036945599"
 };
+const firebase = require("firebase");
 firebase.initializeApp(config);
+
+const loadMainComponet = () => {
+  require('babel-register')(
+    { plugins: 'transform-react-jsx' }
+  );
+  const React = require('react');
+  const ReactDOM = require('react-dom');
+  const MainContent = require('./components/main');
+  const root = document.getElementById('root');
+  ReactDOM.render(React.createElement(MainContent), root);
+}
 
 const signUpBtn = document.getElementById('sign-up');
 const signInBtn = document.getElementById('sign-in');
-// const signInWithGoogleBtn = document.getElementById('sign-in-with-google');
 
 // Sigin in/up with email
 signUpBtn.addEventListener('click', function() {
   const email = document.getElementById('email').value;
+  const displayName = document.getElementById('first-name').value + ' ' + document.getElementById('last-name').value;
   const password = document.getElementById('password').value;
+  const passwordConfirm = document.getElementById('password-confirm').value;
+  if (password != passwordConfirm) {
+    alert('Password does not match. Please confirm your password.');
+    return;
+  }
   firebase.auth().createUserWithEmailAndPassword(email, password).then(function(){
     alert('User created successfully.')
+    const user = firebase.auth().currentUser;
+    user.updateProfile({
+      displayName: displayName
+    }).then(function(){
+      alert('User info updated successfully.');
+      loadMainComponet();
+    }).catch(function(error){
+      console.log(error);
+    });
   }).catch(function(error){
     if(error != null) {
       console.log(error);
@@ -23,20 +55,12 @@ signUpBtn.addEventListener('click', function() {
 });
 
 signInBtn.addEventListener('click', function() {
-  const email = document.getElementById('email').value;
-  const password = document.getElementById('password').value;
-  firebase.auth().signInWithEmailAndPassword(email, password).then(function(){
-    (() => {
-      require('babel-register')(
-        {plugins: 'transform-react-jsx'}
-      );
-      const React = require('react');
-      const ReactDOM = require('react-dom');
-      const MainContent = require('./components/main');
-      const root = document.getElementById('root');
-      ReactDOM.render(React.createElement(MainContent), root);
-    })();
-  }).catch(function(error){
+  const email = document.getElementById('sign-in-email').value;
+  const password = document.getElementById('sign-in-password').value;
+  firebase.auth().signInWithEmailAndPassword(email, password).then(function() {
+    const user = firebase.auth().currentUser;
+    loadMainComponet();
+  }).catch(function(error) {
     if(error != null) {
       console.log(error);
       alert(error.message);
@@ -44,19 +68,3 @@ signInBtn.addEventListener('click', function() {
     }
   });
 });
-
-// Sign in with google account.
-// const provider = new firebase.auth.GoogleAuthProvider();
-// provider.addScope('https://www.googleapis.com/auth/contacts.readonly');
-// signInWithGoogleBtn.addEventListener('click', function() {
-//   firebase.auth().signInWithPopup(provider).then(function(result){
-//     const token = result.credential.accessToken;
-//     const user = result.user;
-//     alert('Sign in with google successfuly');
-//     return;
-//   }).catch(function(error){
-//     console.log(error);
-//     alert(error.message);
-//     return
-//   });
-// });
